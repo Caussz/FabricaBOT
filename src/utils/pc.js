543abc96dab8c1e9a0be686a294ad0ae49251c3f
Caussz <cczcaua@gmail.com>
2023-05-02 16:29:32 -0300

    a

---
+++ src/utils/pc.js
@@ -0,0 +1,623 @@
+const ilhasFabrica = [{
+    posicao: 'A1',
+    aluno: 'Não informado',
+    notebook: false,
+    patrimonio: null,
+    status: 'indisponivel',
+    CPU: 'indisponivel',
+    GPU: 'indisponivel',
+    RAM: 'indisponivel',
+    HS: 'indisponivel',
+    perifericos: {
+      monitor: {
+        marca: 'Não informado',
+        patrimonio: null
+      },
+      teclado: 'Não informado',
+      mause: 'Não informado'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  }, 
+  {
+    posicao: 'A2',
+    aluno: 'Sem aluno',
+    notebook: false,
+    patrimonio: null,
+    status: 'indisponivel',
+    CPU: 'indisponivel',
+    GPU: 'indisponivel',
+    RAM: 'indisponivel',
+    HS: 'indisponivel',
+    perifericos: {
+      monitor: {
+        marca: '',
+        patrimonio: null
+      },
+      teclado: 'Não informado',
+      mause: 'Não informado'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'A3',
+    aluno: 'Joao felipi',
+    notebook: true,
+    patrimonio: null,
+    status: 'Em uso',
+    CPU: 'Não informado',
+    GPU: 'Não informado',
+    RAM: 'Não informado',
+    HS: 'Não informado',
+    perifericos: {
+      monitor: {
+        marca: 'DELL',
+        patrimonio: null
+      },
+      teclado: 'Não informado',
+      mause: 'Não informado'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'A4',
+    aluno: 'Sem aluno',
+    notebook: false,
+    patrimonio: null,
+    status: 'Em manutencao',
+    CPU: 'i5 2600',
+    GPU: 'OFF',
+    RAM: '16gb (12 reconhecido)',
+    HS: '500gb HDD',
+    perifericos: {
+      monitor: {
+        marca: 'BenQ',
+        patrimonio: 007411
+      },
+      teclado: 'Não informado',
+      mause: 'Não informado'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'A5',
+    aluno: 'Sem aluno',
+    notebook: false,
+    patrimonio: null,
+    status: 'Em manutencao',
+    CPU: 'i5 2600',
+    GPU: 'OFF',
+    RAM: '16gb (12 reconhecido)',
+    HS: '500gb HDD',
+    perifericos: {
+      monitor: {
+        marca: 'LG',
+        patrimonio: 003451
+      },
+      teclado: 'Não informado',
+      mause: 'Não informado'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'A6',
+    aluno: 'Matheus Vidal',
+    notebook: false,
+    patrimonio: null,
+    status: 'Em uso',
+    CPU: 'i5 6600',
+    GPU: 'OFF',
+    RAM: '16gb DDR4',
+    HS: '256gb SSD SATA3',
+    perifericos: {
+      monitor: {
+        marca: 'LG',
+        patrimonio: '002281'
+      },
+      teclado: 'Não informado',
+      mause: 'Não informado'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'B1',
+    aluno: 'Sem aluno',
+    notebook: false,
+    patrimonio: null,
+    status: 'Disponivel',
+    CPU: 'i3 4600',
+    GPU: 'gt 730 1gb',
+    RAM: '16gb DDR3',
+    HS: '256gb SSD SATA3',
+    perifericos: {
+      monitor: {
+        marca: 'LG',
+        patrimonio: '003438'
+      },
+      teclado: 'Não informado',
+      mause: 'Não informado'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  }, 
+  {
+    posicao: 'B2',
+    aluno: 'Sem aluno',
+    notebook: false,
+    patrimonio: null,
+    status: 'Disponivel',
+    CPU: 'i5 2600',
+    GPU: 'OFF',
+    RAM: '16gb DDR3',
+    HS: '256gb SSD SATA3',
+    perifericos: {
+      monitor: {
+        marca: 'LG',
+        patrimonio: '2020005700'
+      },
+      teclado: 'Não informado',
+      mause: 'Não informado'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'B3',
+    aluno: 'Sem aluno',
+    notebook: false,
+    patrimonio: null,
+    status: 'Disponivel',
+    CPU: 'i5 6600',
+    GPU: 'GT 730 2gb',
+    RAM: '16gb DDR4',
+    HS: '256gb SSD m2',
+    perifericos: {
+      monitor: {
+        marca: 'LG',
+        patrimonio: '002477'
+      },
+      teclado: 'Não informado',
+      mause: 'Não informado'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'B4',
+    aluno: 'Vinicius Nilo',
+    notebook: false,
+    patrimonio: null,
+    status: 'Em uso',
+    CPU: 'i3 4600',
+    GPU: 'OFF',
+    RAM: '12gb DDR3',
+    HS: '500gb HDD',
+    perifericos: {
+      monitor: {
+        marca: 'LG',
+        patrimonio: '003438'
+      },
+      teclado: 'DELL',
+      mause: 'Multilazer'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'B5',
+    aluno: 'Jose',
+    notebook: false,
+    patrimonio: null,
+    status: 'Em manutencao',
+    CPU: 'i5 2600',
+    GPU: 'OFF',
+    RAM: '8gb DDR3',
+    HS: '500gb HDD',
+    perifericos: {
+      monitor: {
+        marca: 'LG',
+        patrimonio: ''
+      },
+      teclado: 'Não informado',
+      mause: 'Não informado'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'B6',
+    aluno: 'Não informado',
+    notebook: true,
+    patrimonio: null,
+    status: 'Em uso',
+    CPU: 'Não informado',
+    GPU: 'Não informado',
+    RAM: 'Não informado',
+    HS: 'Não informado',
+    perifericos: {
+      monitor: {
+        marca: 'Não informado',
+        patrimonio: null
+      },
+      teclado: 'Não informado',
+      mause: 'Não informado'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'B7',
+    aluno: 'Não informado',
+    notebook: true,
+    patrimonio: null,
+    status: 'Em uso',
+    CPU: 'Não informado',
+    GPU: 'Não informado',
+    RAM: 'Não informado',
+    HS: 'Não informado',
+    perifericos: {
+      monitor: {
+        marca: 'Não informado',
+        patrimonio: null
+      },
+      teclado: 'Não informado',
+      mause: 'Não informado'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'C1',
+    aluno: 'Não informado',
+    notebook: false,
+    patrimonio: null,
+    status: 'Em uso',
+    CPU: 'i5 8600',
+    GPU: 'OFF',
+    RAM: '16gb DDR4',
+    HS: '256gb SSD m2',
+    perifericos: {
+      monitor: {
+        marca: 'HP',
+        patrimonio: null
+      },
+      teclado: 'DELL',
+      mause: 'DELL'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  }, 
+  {
+    posicao: 'C2',
+    aluno: 'Não informado',
+    notebook: false,
+    patrimonio: null,
+    status: 'Em uso',
+    CPU: 'i5 8600',
+    GPU: 'OFF',
+    RAM: '16gb DDR4',
+    HS: '256gb SSD m2',
+    perifericos: {
+      monitor: {
+        marca: 'HP',
+        patrimonio: null
+      },
+      teclado: 'DELL',
+      mause: 'DELL'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'C3',
+    aluno: 'Não informado',
+    notebook: false,
+    patrimonio: null,
+    status: 'Em uso',
+    CPU: 'i5 8600',
+    GPU: 'OFF',
+    RAM: '16gb DDR4',
+    HS: '256gb SSD m2',
+    perifericos: {
+      monitor: {
+        marca: 'HP',
+        patrimonio: null
+      },
+      teclado: 'DELL',
+      mause: 'DELL'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'C4',
+    aluno: 'Não informado',
+    notebook: false,
+    patrimonio: null,
+    status: 'Em uso',
+    CPU: 'i5 8600',
+    GPU: 'OFF',
+    RAM: '16gb DDR4',
+    HS: '256gb SSD m2',
+    perifericos: {
+      monitor: {
+        marca: 'HP',
+        patrimonio: null
+      },
+      teclado: 'DELL',
+      mause: 'DELL'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'C5',
+    aluno: 'Não informado',
+    notebook: false,
+    patrimonio: null,
+    status: 'Em uso',
+    CPU: 'i5 8600',
+    GPU: 'OFF',
+    RAM: '16gb DDR4',
+    HS: '256gb SSD m2',
+    perifericos: {
+      monitor: {
+        marca: 'HP',
+        patrimonio: null
+      },
+      teclado: 'DELL',
+      mause: 'DELL'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'C6',
+    aluno: 'Não informado',
+    notebook: false,
+    patrimonio: null,
+    status: 'Em uso',
+    CPU: 'i5 8600',
+    GPU: 'OFF',
+    RAM: '16gb DDR4',
+    HS: '256gb SSD m2',
+    perifericos: {
+      monitor: {
+        marca: 'HP',
+        patrimonio: null
+      },
+      teclado: 'DELL',
+      mause: 'DELL'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'D1',
+    aluno: 'Não informado',
+    notebook: false,
+    patrimonio: null,
+    status: 'Em uso',
+    CPU: 'i5 8600',
+    GPU: 'OFF',
+    RAM: '16gb DDR4',
+    HS: '256gb SSD m2',
+    perifericos: {
+      monitor: {
+        marca: 'HP',
+        patrimonio: null
+      },
+      teclado: 'DELL',
+      mause: 'DELL'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  }, 
+  {
+    posicao: 'D2',
+    aluno: 'Gustavo Gorges',
+    notebook: false,
+    patrimonio: null,
+    status: 'Em uso',
+    CPU: 'i5 8600',
+    GPU: 'OFF',
+    RAM: '16gb DDR4',
+    HS: '256gb SSD SATA3',
+    perifericos: {
+      monitor: {
+        marca: 'HP',
+        patrimonio: null
+      },
+      teclado: 'DELL',
+      mause: 'DELL'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'D3',
+    aluno: 'Caua Vinicius',
+    notebook: false,
+    patrimonio: null,
+    status: 'Em uso',
+    CPU: 'i5 6600',
+    GPU: 'GT 730 2gb (nova)',
+    RAM: '16gb DDR4',
+    HS: '256gb SSD m2',
+    perifericos: {
+      monitor: {
+        marca: 'LG',
+        patrimonio: null
+      },
+      teclado: 'DELL',
+      mause: 'DELL'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'D4',
+    aluno: 'Gabi',
+    notebook: false,
+    patrimonio: null,
+    status: 'Em uso',
+    CPU: 'i5 8600',
+    GPU: 'OFF',
+    RAM: '16gb DDR4',
+    HS: '256gb SSD m2',
+    perifericos: {
+      monitor: {
+        marca: 'HP',
+        patrimonio: null
+      },
+      teclado: 'DELL',
+      mause: 'DELL'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'D5',
+    aluno: 'Souethe',
+    notebook: true,
+    patrimonio: null,
+    status: 'Em uso',
+    CPU: 'Não informado',
+    GPU: 'Não informado',
+    RAM: 'Não informado',
+    HS: 'Não informado',
+    perifericos: {
+      monitor: {
+        marca: 'HP',
+        patrimonio: null
+      },
+      teclado: 'DELL',
+      mause: 'DELL'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'D6',
+    aluno: 'Dacio',
+    notebook: true,
+    patrimonio: null,
+    status: 'Em uso',
+    CPU: 'Não informado',
+    GPU: 'Não informado',
+    RAM: 'Não informado',
+    HS: 'Não informado',
+    perifericos: {
+      monitor: {
+        marca: 'Não informado',
+        patrimonio: null
+      },
+      teclado: 'Não informado',
+      mause: 'Não informado'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'E1',
+    aluno: 'Não informado',
+    notebook: false,
+    patrimonio: null,
+    status: 'indisponivel',
+    CPU: 'Não informado',
+    GPU: 'Não informado',
+    RAM: 'Não informado',
+    HS: 'Não informado',
+    perifericos: {
+      monitor: {
+        marca: 'Não informado',
+        patrimonio: null
+      },
+      teclado: 'Não informado',
+      mause: 'Não informado'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  }, 
+  {
+    posicao: 'E2',
+    aluno: 'Não informado',
+    notebook: false,
+    patrimonio: null,
+    status: 'indisponivel',
+    CPU: 'Não informado',
+    GPU: 'Não informado',
+    RAM: 'Não informado',
+    HS: 'Não informado',
+    perifericos: {
+      monitor: {
+        marca: 'Não informado',
+        patrimonio: null
+      },
+      teclado: 'Não informado',
+      mause: 'Não informado'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'E3',
+    aluno: 'Não informado',
+    notebook: false,
+    patrimonio: null,
+    status: 'indisponivel',
+    CPU: 'Não informado',
+    GPU: 'Não informado',
+    RAM: 'Não informado',
+    HS: 'Não informado',
+    perifericos: {
+      monitor: {
+        marca: 'Não informado',
+        patrimonio: null
+      },
+      teclado: 'Não informado',
+      mause: 'Não informado'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'E4',
+    aluno: 'Não informado',
+    notebook: false,
+    patrimonio: null,
+    status: 'indisponivel',
+    CPU: 'Não informado',
+    GPU: 'Não informado',
+    RAM: 'Não informado',
+    HS: 'Não informado',
+    perifericos: {
+      monitor: {
+        marca: 'Não informado',
+        patrimonio: null
+      },
+      teclado: 'Não informado',
+      mause: 'Não informado'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'E5',
+    aluno: 'Não informado',
+    notebook: false,
+    patrimonio: null,
+    status: 'indisponivel',
+    CPU: 'Não informado',
+    GPU: 'Não informado',
+    RAM: 'Não informado',
+    HS: 'Não informado',
+    perifericos: {
+      monitor: {
+        marca: 'Não informado',
+        patrimonio: ''
+      },
+      teclado: 'Não informado',
+      mause: 'Não informado'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  {
+    posicao: 'E6',
+    aluno: 'Não informado',
+    notebook: false,
+    patrimonio: null,
+    status: 'indisponivel',
+    CPU: 'Não informado',
+    GPU: 'Não informado',
+    RAM: 'Não informado',
+    HS: 'Não informado',
+    perifericos: {
+      monitor: {
+        marca: 'Não informado',
+        patrimonio: null
+      },
+      teclado: 'Não informado',
+      mause: 'Não informado'
+    },
+    last: 'Última modificação no dia 05/04/2023'
+  },
+  ]
+
+module.exports = ilhasFabrica
